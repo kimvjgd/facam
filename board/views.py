a04e1c529f47a426183ef97f7a677a6534705f42
dongpakka <kimvjgd@naver.com>
2022-02-07 00:43:31 +0900

    blocked...

--- board/views.py
+++ board/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from board.forms import BoardForm
+from .models import Board
+
 # Create your views here.
+
+def board_write(request):
+    form = BoardForm()
+    return render(request, 'board_write.html', {'form': form})
+
+def board_list(request):
+    boards = Board.objects.all().order_by('-id')
+    return render(request, 'board_list.html', {'boards': boards})
--- fcuser/models.py
+++ fcuser/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Fcuser(models.Model):
     username = models.CharField(max_length=32,
                                 verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,
+                                 verbose_name='사용자이메일')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registerer_dttm = models.DateTimeField(auto_now_add=True,
